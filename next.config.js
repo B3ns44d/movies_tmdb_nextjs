d03504ac172246aeeb034106b4411bfb822c908b
B3ns44d <abdsamad23bnsaad@gmail.com>
2021-04-22 10:51:37 +0000

    :wrench: update configuration files.
    add utils path, next domains for api images, add some custom tailwindcss config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   images: {
-    domains: ["press.hulu.com"],
+    domains: ["press.hulu.com", "api.themovied.org", "image.tmdb.org"],
   },
   future: {
     webpack5: true,
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
 module.exports = {
-  mode: 'jit',
+  mode: "jit",
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "3xl": "2000px",
+      }
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      transitionDuration: ["hover", "focus"],
+      backgroundColor: ["active"],
+      divideColor: ["group-hover"],
+      backgroundColor: ["active"],
+    },
   },
-  plugins: [],
+  plugins: [require("tailwind-scrollbar-hide")],
 };
